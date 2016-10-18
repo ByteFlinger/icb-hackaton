10b150197db18ee60ddbe0e196a5b343cd059c73
Alexandre Thenorio <alexandre.thenorio@ericsson.com>
2016-10-18 12:16:02 +0200

    First attempt at Bot framework bot

--- node-bot/app.js
+++ node-bot/app.js
@@ -1,25 +1,55 @@
 'use strict';
 
 const builder = require('botbuilder');
-const express = require('express');
+const restify = require('restify');
 
-const connector = new builder.ChatConnector();
+// Create chat bot
+let connector = new builder.ConsoleConnector().listen();
+// let connector = new builder.ChatConnector({
+//     appId: process.env.MICROSOFT_APP_ID,
+//     appPassword: process.env.MICROSOFT_APP_PASSWORD
+// });
+
+// let server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, function() {
+//     console.log('%s listening to %s', server.name, server.url);
+// });
+// server.post('/api/messages', connector.listen());
 
 let bot = new builder.UniversalBot(connector);
 
-// endpoints for the bot
-// bot.dialog('/', (session) => {
-//   session.send('you said', session.message);
-// });
+const LuisModelUrl = process.env.LUIS_MODEL_URL;
 
-// waterfall endpoint
-bot.dialog('/', [
-  (session) => { builder.Prompts.text(session, 'Please enter your name'); },
-  (session, result) => { session.send('your name is '+result.response); }
-]);
+// Main dialog with LUIS
+let recognizer = new builder.LuisRecognizer(LuisModelUrl);
+let intents = new builder.IntentDialog({
+    recognizers: [recognizer]
+});
+
+bot.dialog('/', intents);
+
+// Add intent handlers
+intents.matches("book_room", [
+    function(session, args, next) {
+        // Resolve and store any entities passed from LUIS.
+        let site = builder.EntityRecognizer.findEntity(args.entities, "site");
+        if (!site) {
+            builder.Prompts.text(session, "Which site?");
+        } else {
+            next({
+                response: site.entity
+            });
+        }
+      },
+    function (session, results) {
+        if (results.response) {
+            // ... save task
+            session.send("Ok... Added the '%s' task.", results.response);
+        } else {
+            session.send("Ok");
+        }
+    }
+  ]);
 
 
-let app = express();
-app.use((req, res) => res.status(404).json({ error: 'Not Found' }));
-
-module.exports = app;
+intents.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));

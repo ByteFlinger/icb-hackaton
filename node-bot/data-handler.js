ca5e0d560b7bbcaa7b17a5b7539a54145244a9f5
aure <aurelien.hontabat@ericsson.com>
2016-10-18 20:22:39 +0200

    Add getRoomState and getSites functions

--- node-bot/data-handler.js
+++ node-bot/data-handler.js
@@ -28,21 +28,53 @@ exports.getAvailableRooms = (options, cb) => {
 };
 
 
-/*exports.getSites = () => {
+/**
+ * returns an array containing the sites
+ * 
+ * @usage
+ * getRoomState('6610 Skyskrapan', (room) => {
+    console.log(room);
+  });
+ * 
+ */
+exports.getRoomState = (name, cb) => {
   request.get('http://52.57.171.54:8080/rooms', (error, response) => {
     if (!error && response.statusCode === 200) {
       let data = JSON.parse(response.body);
-
-      data.forEach();
-
-
-      let matchOptions = (room) => {
-        return !room.booking && (room.site === (options.site || room.site)) && (room.floor === (options.floor || room.floor));
-      };
-      cb(data.filter(matchOptions));
+      let trigger = false;
+      data.forEach((room) => {
+        if(room.name === name && !trigger){ cb(room); trigger = true; }
+      }); 
     } else {
       console.log('GET request failed', error);
     }
   }).auth('user', 'hackathon', false);
-};*/
+};
+
+
+/**
+ * returns an array containing the sites
+ * 
+ * @usage
+ * getSites((sites)=>{
+    console.log(sites);
+  });
+ * 
+ */
+exports.getSites = (cb) => {
+  request.get('http://52.57.171.54:8080/rooms', (error, response) => {
+    if (!error && response.statusCode === 200) {
+      let data = JSON.parse(response.body);
+      let results = [];
+      data.forEach((room) => {
+        if(!results.includes(room.site)) results.push(room.site);
+      });
+      cb(results);
+    } else {
+      console.log('GET request failed', error);
+    }
+  }).auth('user', 'hackathon', false);
+};
+
+
 

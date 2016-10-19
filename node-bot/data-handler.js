f42f7196afef567d74c2814409e8982590814b43
Alexandre Thenorio <alexandre.thenorio@ericsson.com>
2016-10-19 03:03:18 +0200

    Promises everywhere

--- node-bot/data-handler.js
+++ node-bot/data-handler.js
@@ -12,20 +12,40 @@ const levenshtein = require('fast-levenshtein');
   });
  *
  */
-exports.getAvailableRooms = (options, cb) => {
+// exports.getAvailableRooms = (options, cb) => {
+//     options = options ? options : {};
+//
+//     request.get('http://52.57.171.54:8080/rooms', (error, response) => {
+//         if (!error && response.statusCode === 200) {
+//             let data = JSON.parse(response.body);
+//             let matchOptions = (room) => {
+//                 return !room.booking && (room.site === (options.site || room.site)) && (room.floor === (options.floor || room.floor));
+//             };
+//             cb(data.filter(matchOptions));
+//         } else {
+//             console.log('GET request failed', error);
+//         }
+//     }).auth('user', 'hackathon', false);
+// };
+
+exports.getAvailableRooms = (options) => {
     options = options ? options : {};
 
-    request.get('http://52.57.171.54:8080/rooms', (error, response) => {
-        if (!error && response.statusCode === 200) {
-            let data = JSON.parse(response.body);
-            let matchOptions = (room) => {
-                return !room.booking && (room.site === (options.site || room.site)) && (room.floor === (options.floor || room.floor));
-            };
-            cb(data.filter(matchOptions));
-        } else {
-            console.log('GET request failed', error);
-        }
-    }).auth('user', 'hackathon', false);
+    return new Promise(function(resolve, reject) {
+
+        request.get('http://52.57.171.54:8080/rooms', (error, response) => {
+            if (!error && response.statusCode === 200) {
+                let data = JSON.parse(response.body);
+                let matchOptions = (room) => {
+                    return !room.booking && (room.site === (options.site || room.site)) && (room.floor === (options.floor || room.floor));
+                };
+                resolve(data.filter(matchOptions));
+            } else {
+                reject(error);
+                console.log('GET request failed', error);
+            }
+        }).auth('user', 'hackathon', false);
+    });
 };
 
 
@@ -55,19 +75,37 @@ exports.getRoomState = (name, cb) => {
     }).auth('user', 'hackathon', false);
 };
 
-exports.getSiteFloorList = (site, cb) => {
-    request.get('http://52.57.171.54:8080/rooms', (error, response) => {
-        if (!error && response.statusCode === 200) {
-            let data = JSON.parse(response.body);
-            let results = [];
-            data.forEach((room) => {
-                if (room.site === site && !results.includes(room.floor)) results.push(room.floor);
-            });
-            cb(results);
-        } else {
-            console.log('GET request failed', error);
-        }
-    }).auth('user', 'hackathon', false);
+// exports.getSiteFloorList = (site, cb) => {
+//     request.get('http://52.57.171.54:8080/rooms', (error, response) => {
+//         if (!error && response.statusCode === 200) {
+//             let data = JSON.parse(response.body);
+//             let results = [];
+//             data.forEach((room) => {
+//                 if (room.site === site && !results.includes(room.floor)) results.push(room.floor);
+//             });
+//             cb(results);
+//         } else {
+//             console.log('GET request failed', error);
+//         }
+//     }).auth('user', 'hackathon', false);
+// };
+
+exports.getSiteFloorList = (site) => {
+    return new Promise(function(resolve, reject) {
+        request.get('http://52.57.171.54:8080/rooms', (error, response) => {
+            if (!error && response.statusCode === 200) {
+                let data = JSON.parse(response.body);
+                let results = [];
+                data.forEach((room) => {
+                    if (room.site === site && !results.includes(room.floor)) results.push(room.floor);
+                });
+                resolve(results);
+            } else {
+                console.log('GET request failed', error);
+                reject(error);
+            }
+        }).auth('user', 'hackathon', false);
+    });
 };
 
 
@@ -80,19 +118,38 @@ exports.getSiteFloorList = (site, cb) => {
   });
  *
  */
-exports.getSites = (cb) => {
-    request.get('http://52.57.171.54:8080/rooms', (error, response) => {
-        if (!error && response.statusCode === 200) {
-            let data = JSON.parse(response.body);
-            let results = [];
-            data.forEach((room) => {
-                if (!results.includes(room.site)) results.push(room.site);
-            });
-            cb(results);
-        } else {
-            console.log('GET request failed', error);
-        }
-    }).auth('user', 'hackathon', false);
+// exports.getSites = (cb) => {
+//     request.get('http://52.57.171.54:8080/rooms', (error, response) => {
+//         if (!error && response.statusCode === 200) {
+//             let data = JSON.parse(response.body);
+//             let results = [];
+//             data.forEach((room) => {
+//                 if (!results.includes(room.site)) results.push(room.site);
+//             });
+//             cb(results);
+//         } else {
+//             console.log('GET request failed', error);
+//         }
+//     }).auth('user', 'hackathon', false);
+// };
+
+exports.getSites = () => {
+
+    return new Promise(function(resolve, reject) {
+        request.get('http://52.57.171.54:8080/rooms', (error, response) => {
+            if (!error && response.statusCode === 200) {
+                let data = JSON.parse(response.body);
+                let results = [];
+                data.forEach((room) => {
+                    if (!results.includes(room.site)) results.push(room.site);
+                });
+                resolve(results);
+            } else {
+                console.log('GET request failed', error);
+                reject(error);
+            }
+        }).auth('user', 'hackathon', false);
+    });
 };
 
 
@@ -101,14 +158,12 @@ exports.findClosestMatch = function(text, matchArray) {
     let levDistance = 999999;
     console.log(matchArray);
     for (let i = 0; i < matchArray.length; i++) {
-        console.log("Loop " + i);
         let distance = levenshtein.get(text, matchArray[i]);
-        console.log("Distance between " + text + " - " + matchArray[i] + " = " + distance);
         if (distance < levDistance) {
             match = matchArray[i];
             levDistance = distance;
         }
     }
-    console.log("Got back " + match);
+    console.log("Closest Lev match was " + match);
     return match;
 };
--- node-bot/intents/info-available-rooms.js
+++ node-bot/intents/info-available-rooms.js
@@ -1,92 +1,145 @@
 'use strict';
 
 const dataHandler = require('../data-handler');
-const async = require('async');
+
+function getSite(builder, entities) {
+    let siteEntity = builder.EntityRecognizer.findEntity(entities, "site");
+    let site;
+
+    return new Promise(function(resolve, reject) {
+        if (siteEntity) {
+            dataHandler.getSites().then(function(sites) {
+                    console.log("Checking for actual site");
+                    console.log("Got asked for " + siteEntity.entity);
+                    let match = builder.EntityRecognizer.findBestMatch(sites, siteEntity.entity);
+                    if (match) {
+                        console.log("Framework match would have been " + match.entity);
+                    } else {
+                        console.log("No match found by bot framework");
+                    }
+                    let actualSite = dataHandler.findClosestMatch(siteEntity.entity, sites);
+                    if (actualSite) {
+                        site = actualSite;
+                    }
+                    resolve(site);
+                },
+                function(err) {
+                    resolve(site);
+                });
+        } else {
+            resolve(site);
+        }
+    });
+}
+
+function getFloor(builder, entities, site) {
+    let floorEntity = builder.EntityRecognizer.findEntity(entities, "floor");
+    let floor;
+
+    return new Promise(function(resolve, reject) {
+        console.log("Checking for specific floor");
+        if (floorEntity) {
+            console.log(`Asked for floor ${floorEntity.entity}`);
+            if (site) {
+                console.log(`Finding best match for site ${site}`);
+                return dataHandler.getSiteFloorList(site).then(function(floors) {
+                        let actualFloor = dataHandler.findClosestMatch(floorEntity.entity, floors);
+                        if (actualFloor) {
+                            floor = actualFloor;
+                        }
+                        resolve(floor);
+                    },
+                    function(err) {
+                        resolve(floor);
+                    });
+            } else {
+                resolve(floor);
+            }
+        } else {
+            resolve(floor);
+        }
+
+    });
+}
+
+function getRoomType(builder, entities) {
+    let roomTypeEntity = builder.EntityRecognizer.findEntity(entities, "room_type");
+    let roomType;
+    return new Promise(function(resolve, reject) {
+        if (roomTypeEntity) {
+            roomType = roomTypeEntity.entity;
+        }
+        resolve(roomType);
+    });
+}
 
 module.exports = (intent, builder) => {
 
     intent.matches("info_available_rooms", [
         function(session, args, next) {
-            // Resolve and store any entities passed from LUIS.
-            let siteEntity = builder.EntityRecognizer.findEntity(args.entities, "site");
-            let floorEntity = builder.EntityRecognizer.findEntity(args.entities, "floor");
-            let roomEntity = builder.EntityRecognizer.findEntity(args.entities, "room");
-            let roomTypeEntity = builder.EntityRecognizer.findEntity(args.entities, "room_type");
-            let roomTemperatureEntity = builder.EntityRecognizer.findEntity(args.entities, "room_temperature");
+            // For demo purposes we wipe userdata
+            session.userData.site = undefined;
+            session.userData.roomType = undefined;
+            session.userData.floor = undefined;
+            let requestSite;
+            let requestFloor;
+            let requestRoomType;
 
-            async.series([
-                function(callback) {
-                    if (!roomEntity) {
-                        // For demo purposes we wipe the site before
-                        session.userData.site = undefined;
-                        if (siteEntity) {
-                            dataHandler.getSites((sites) => {
-                                console.log("Checking for actual site");
-                                console.log("Got asked for " + siteEntity.entity);
-                                let actualSite = dataHandler.findClosestMatch(siteEntity.entity, sites);
-                                if (actualSite) {
-                                    session.userData.site = actualSite;
-                                }
-                                callback(null, 'one');
-                            });
-                        }
-                    }
-                },
-                function(callback) {
-                    if (floorEntity) {
-                        console.log("Setting entity");
-                        if (session.userData.site) {
-                            dataHandler.getSiteFloorList(session.userData.site, (floors) => {
-                                let actualFloor = dataHandler.findClosestMatch(floorEntity.entity, floors);
-                                if (actualFloor) {
-                                    session.userData.floor = actualFloor;
-                                }
-                                callback(null, 'two');
-                            });
-                        }
-                    }
-                },
-                function(callback) {
-                    // Maybe not save room type on the user but just this dialog
-                    if (roomTypeEntity) {
-                        session.dialogData.roomType = roomTypeEntity.entity;
-                    }
-                    callback(null, 'three');
-                },
-                function(callback) {
-                    if (!session.userData.site && !session.userData.floor && !session.dialogData.roomType) {
-                        console.log("Getting site choice");
-                        dataHandler.getSites((sites) => {
-                            console.log(sites);
-                            builder.Prompts.choice(session, "Care to specify the location? Or don't, I don't care much for such things", sites);
-                        });
-
-                        // builder.Prompts.choice(session, "Care to specify where you want to book the room? Or don't, I don't care much for such things", ["Hello"]);
-                    } else if (!siteEntity && session.userData.site) {
-                        //Find room in previous site
-                        session.send(`I suppose you expect me to remember your last site ${session.userData.site}. I hope I find no available rooms in it`);
-                        next();
-                    } else {
-                        next();
-                        // Query for room
-                        // console.log(`Would have queried for room in ${session.userData.site}, floor $${session.userData.floor} and type ${session.userData.rtype}`);
-                        // session.endDialog(`Would have queried for room in ${session.userData.site}, floor ${session.userData.floor} and type ${session.userData.rtype} but people can be bothered to teach poor old marvin`);
-                    }
-                    callback(null, 'four');
+            getSite(builder, args.entities).then(function(site) {
+                if (site) {
+                    requestSite = site;
                 }
-            ]);
 
-            // if (!site) {
-            //     builder.Prompts.text(session, "Which site?");
-            // } else {
-            //     next({
-            //         response: site.entity
-            //     });
-            // }
+                return getFloor(builder, args.entities, site);
+            }).then(function(floor) {
+                if (floor) {
+                    requestFloor = floor;
+                }
+
+                return getRoomType(builder, args.entities);
+            }).then(function(roomType) {
+                if (roomType) {
+                    requestRoomType = roomType;
+                }
+            }).then(function() {
+                if (requestSite) {
+                    session.userData.site = requestSite;
+                }
+
+                if (requestFloor) {
+                    session.userData.floor = requestFloor;
+                }
+
+                if (requestRoomType) {
+                    session.dialogData.roomType = requestRoomType;
+                }
+
+                // console.log("Site: " + session.userData.site);
+                // console.log("Floor: " + session.userData.floor);
+                // console.log("User RoomType: " + session.userData.roomType);
+                // console.log("RoomType: " + session.dialogData.roomType);
+                if (!session.userData.site && !session.userData.floor && !session.dialogData.roomType) {
+                    console.log("Getting site choice");
+                    dataHandler.getSites().then(function(sites) {
+                        console.log(sites);
+                        builder.Prompts.choice(session, "Care to specify the location? Or don't, I don't care much for such things", sites);
+                    });
+
+                    // builder.Prompts.choice(session, "Care to specify where you want to book the room? Or don't, I don't care much for such things", ["Hello"]);
+                } else if (!requestSite && session.userData.site) {
+                    //Find room in previous site
+                    session.send(`I suppose you expect me to remember your last site ${session.userData.site}. I hope I find no available rooms in it`);
+                    next();
+                } else {
+                    next();
+                    // Query for room
+                    // console.log(`Would have queried for room in ${session.userData.site}, floor $${session.userData.floor} and type ${session.userData.rtype}`);
+                    // session.endDialog(`Would have queried for room in ${session.userData.site}, floor ${session.userData.floor} and type ${session.userData.rtype} but people can be bothered to teach poor old marvin`);
+                }
+            });
         },
         function(session, results) {
             if (results.response) {
-                console.log("Found site");
                 session.userData.site = results.response.entity;
             }
 
@@ -108,8 +161,7 @@ module.exports = (intent, builder) => {
                 notFoundStr += "**Room Type**: " + filter.roomType + "\n\n";
             }
 
-            dataHandler.getAvailableRooms(filter, (rooms) => {
-                console.log(rooms);
+            dataHandler.getAvailableRooms(filter).then(function(rooms) {
                 if (rooms !== undefined && rooms.length > 0) {
                     let room = rooms[0];
                     session.endDialog(`There are rooms available\n\n**Site**: ${room.site}\n\n**Floor**: ${room.floor}\n\n**Name**: ${room.name}\n\nI hope the lights don't work`);
@@ -117,8 +169,6 @@ module.exports = (intent, builder) => {
                     session.endDialog(`There are no rooms available for the following.\n\n${notFoundStr}I can look in the next town but you won't like it`);
                 }
             });
-
-            // session.endDialog(`There are rooms available\n\n**Site**: Value\n\n**Floor**: Value\n\n**Name**: Value\n\nI hope the lights don't work`);
         }
     ]);
 };

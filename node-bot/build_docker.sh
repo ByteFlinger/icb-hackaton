585d0d41c5ca128a7b1847aa306ea3b9ac4a4a2d
ByteFlinger <byteflinger@gmail.com>
2016-10-20 20:47:00 +0200

    Should never have pushed the changed in build script

--- node-bot/build_docker.sh
+++ node-bot/build_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build --build-arg HTTP_PROXY=http://172.17.0.1:3128 --build-arg http_proxy=http://172.17.0.1:3128 --build-arg HTTPS_PROXY=htt://172.17.0.1:3128 --build-arg https_proxy=http://172.17.0.1:3128 -t marvin .
+#docker build --build-arg HTTP_PROXY=http://172.17.0.1:3128 --build-arg http_proxy=http://172.17.0.1:3128 --build-arg HTTPS_PROXY=htt://172.17.0.1:3128 --build-arg https_proxy=http://172.17.0.1:3128 -t marvin .
+
+docker build -t marvin .
